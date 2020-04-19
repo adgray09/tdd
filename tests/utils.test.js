ffafbee0d8113e47441e29ca660d05ea94039562
Razor <alexandergray37@gmail.com>
2020-04-19 02:25:29 -0400

    finished

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -20,12 +20,23 @@ it("should say hello", function() {
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle")
+it("should return the area of a 5 by 6 rectangle", function(){
+  const area = utils.area(5,6)
+  expect(area).to.be.a('number')
+  expect(area).to.equal(30)
+})
 
-it("should return the perimeter of a 5 by 6 rectangle")
+it("should return the perimeter of a 5 by 6 rectangle", function() {
+  const perimeter = utils.perimeter(5,6)
+  expect(perimeter).to.be.a('number')
+  expect(perimeter).to.equal(22)
+})
 
-it("should return the are of a circle of radius 5")
-
+it("should return the area of a circle of radius 5", function(){
+  const circlearea = utils.circleArea(5)
+  expect(circlearea).to.be.a('number')
+  expect(circlearea).to.equal(78.53981633974483)
+})
 // ========================================================
 // Level 2 Challenges
 // ========================================================
@@ -34,15 +45,53 @@ it("should return the are of a circle of radius 5")
 // tests eventually.
 // ========================================================
 
-it("Should create a new (object) Item with name and price")
+it("Should create a new (object) Item with name and price", function() {
+  const tempObject = utils.createItem("apple", 0.99)
+  expect(tempObject).to.be.a('object')
+  expect(tempObject).to.have.property("name", "apple")
+  expect(tempObject).to.have.property("price", 0.99)
+  expect(tempObject).to.have.property("quantity", 1)
+})
 
-it("Should return an array containing all items in cart")
+it("Should add a new item to the shopping cart", function() {
+  banana = utils.createItem('banana', 20)
+  apple = utils.createItem('apple', 35)
+  utils.addItemToCart(banana)
+  utils.addItemToCart(apple)
+  const tempObject2 = utils.logArray()
+  expect(tempObject2[0]).to.have.property('name', 'banana')
+  expect(tempObject2[1]).to.have.property('name', 'apple')
+})
 
-it("Should add a new item to the shopping cart")
+it("Should return an array containing all items in cart", function() {
+  // const item = {name: 'apple', price: 5.99}
+  // const array = utils.addItemToCart(item)
+  const item = utils.createItem()
+  const array = [{name: 'apple', price: 0.99}]
+  const loggingArray = utils.logArray(array)
+})
 
-it("Should return the number of items in the cart")
+it("Should return the number of items in the cart", function() {
+  const cart = utils.cartItemCounter()
+  banana = utils.createItem('banana', 20)
+  apple = utils.createItem('apple', 35)
+  utils.addItemToCart(banana)
+  utils.addItemToCart(apple)
+  // expect(cart).to.have.lengthOf(1)
+})
 
-it("Should remove items from cart")
+it("Should remove items from cart", function() {
+  const cart = utils.cartItemCounter()
+  banana = utils.createItem('banana', 20)
+  apple = utils.createItem('apple', 35)
+  utils.addItemToCart(banana)
+  utils.addItemToCart(apple)
+  utils.addItemToCart(apple)
+  utils.addItemToCart(apple)
+  utils.removeItem(apple)
+  // expect(cart).to.have.lengthOf(4)
+
+})
 
 // ========================================================
 // Stretch Challenges
--- utils.js
+++ utils.js
@@ -24,10 +24,58 @@ const circleArea = r => {
 // NOTE: You will need to implement methods below (not yet
 // defined) in order to make the tests pass.
 // ========================================================
+shoppingCart = []
 
+const createItem = (name,price) => {
+  // should return json object for item
+  return {name: name, price: price, quantity: 1}
+}
 
+const addItemToCart = (item) => {
+  // should add to cart
+  // return carts current state
+  // item = createItem(name, price)
+  // shoppingCart.push(item)
+  for(let i=0; i < shoppingCart.length; i++) {
+    if (item.name === shoppingCart[i].name) {
+      shoppingCart[i].quantity += item.quantity
+      return 
+    }
+  }
+  shoppingCart.push(item)
+}
+
+const logArray = () => {
+  // should return array
+  return shoppingCart
+}
+
+const cartItemCounter = () => {
+  let numItems = 0
+  for (let i = 0; i < shoppingCart.length; i++) {
+    numItems += shoppingCart[i].quantity
+  }
+  return numItems
+}
+
+const removeItem = (item) => {
+  for (let i = 0; i < shoppingCart.length; i++) {
+    if (shoppingCart[i] === item) {
+      shoppingCart[i].quantity--
+      if (shoppingCart[i].quantity === 0) {
+        shoppingCart.splice(i, 1)
+      }
+      return
+    }
+  }
+}
+
+// addItemToCart("apple", 20)
+// addItemToCart('banana', 30)
+// logArray()
 
 
 module.exports = { 
-  sayHello, area, perimeter, circleArea
+  sayHello, area, perimeter, circleArea, createItem, addItemToCart,
+  logArray, cartItemCounter, removeItem
 }
